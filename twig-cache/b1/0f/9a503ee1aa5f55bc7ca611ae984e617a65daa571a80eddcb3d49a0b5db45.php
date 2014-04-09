e6866d134043dfc41d218d8836688e3c8a2898dd
laurabunea <laura.r.bunea@gmail.com>
2014-04-09 15:26:34 +1200

    added a panel lego block in

--- twig-cache/b1/0f/9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b5db45.php
+++ twig-cache/b1/0f/9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b5db45.php
@@ -40,51 +40,54 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
         $context["gallery2Items"] = array(0 => array("caption" => "Item one", "image" => "/assets/images/gallery-showcase.jpg"), 1 => array("caption" => "Item two", "image" => "/assets/images/gallery-showcase.jpg"));
         // line 29
         echo "    ";
+        $context["panel"] = array("title" => "Get in touch", "desc" => "We would love to hear from you, you attractive person you.", "btnLink" => "helpme.co.nz", "btnTitle" => "Contact Us");
+        // line 38
+        echo "    ";
         $context["commentThread"] = array(0 => array("name" => "Laura Bunea", "comment" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices, elit sed faucibus pharetra, diam mauris bibendum orci, sit amet ullamcorper purus dui sit amet augue. Donec aliquet diam ut neque mattis, eu tristique sem rutrum."), 1 => array("name" => "Mr Blob", "comment" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices, elit sed faucibus pharetra, diam mauris bibendum orci, sit amet ullamcorper purus dui sit amet augue. Donec aliquet diam ut neque mattis, eu tristique sem rutrum."));
-        // line 40
+        // line 49
         echo "    ";
         $context["gallery3Items"] = array(0 => array("caption" => "Item one", "image" => "/assets/images/gallery-showcase.jpg"), 1 => array("caption" => "Item two", "image" => "/assets/images/gallery-showcase.jpg"), 2 => array("caption" => "Item three", "image" => "/assets/images/gallery-showcase.jpg"));
-        // line 55
+        // line 64
         echo "    
     ";
-        // line 56
+        // line 65
         $context["gallery4Items"] = array(0 => array("caption" => "Item one", "image" => "/assets/images/gallery-showcase.jpg"), 1 => array("caption" => "Item two", "image" => "/assets/images/gallery-showcase.jpg"), 2 => array("caption" => "Item three", "image" => "/assets/images/gallery-showcase.jpg"), 3 => array("caption" => "Item four", "image" => "/assets/images/gallery-showcase.jpg"));
-        // line 75
+        // line 84
         echo "
     ";
-        // line 76
+        // line 85
         $context["navigation"] = array(0 => array("title" => "Home"), 1 => array("title" => "About"), 2 => array("title" => "Blog"));
-        // line 88
-        echo "
-    ";
-        // line 89
-        $context["gallerywithcaptions"] = array("title" => "Gallery images with captions", "image" => "/assets/images/gallery-showcase.jpg", "imagetitle" => "Image title", "imagesubtitle" => "Image sub title or date", "imagecaption" => "I am an image caption");
-        // line 96
-        echo "
-    ";
         // line 97
+        echo "
+    ";
+        // line 98
+        $context["gallerywithcaptions"] = array("title" => "Gallery images with captions", "image" => "/assets/images/gallery-showcase.jpg", "imagetitle" => "Image title", "imagesubtitle" => "Image sub title or date", "imagecaption" => "I am an image caption");
+        // line 105
+        echo "
+    ";
+        // line 106
         $context["content2Items"] = array(0 => array("heading" => "Heading 1", "copy" => "Anyway, Hairy Maclary from Donaldsons Dairy is just The Hungery Caterpilar in disguise, to find the true meaning of life, one must start chundering with the can of Watties Baked Beans, mate. After the vivid is munted, you add all the pearler wet blankets to the giant weka youve got yourself a meal. ", "link" => "Click me", "link-href" => "http:www.clickme.co.nz"), 1 => array("heading" => "Heading 2", "copy" => "You have no idea how nuclear-free our hard case stubbies were aye. Every time I see those good as cans of Watties Baked Beans its like smoko time all over again aye, Im not here to frack spiders. Anyway, Rangi is just Sir Edmond Hillary in disguise, to find the true meaning of life, one must start burning my Vogels with the Tui, mate.", "link" => "Click me", "link-href" => "http:www.clickme.co.nz"));
-        // line 109
+        // line 118
         echo "
     ";
-        // line 110
+        // line 119
         $context["content3Items"] = array(0 => array("heading" => "Heading 1", "copy" => "Anyway, Hairy Maclary from Donaldsons Dairy is just The Hungery Caterpilar in disguise, to find the true meaning of life, one must start chundering with the can of Watties Baked Beans, mate. After the vivid is munted, you add all the pearler wet blankets to the giant weka youve got yourself a meal. ", "link" => "Click me", "link-href" => "http:www.clickme.co.nz"), 1 => array("heading" => "Heading 2", "copy" => "You have no idea how nuclear-free our hard case stubbies were aye. Every time I see those good as cans of Watties Baked Beans its like smoko time all over again aye, Im not here to frack spiders. Anyway, Rangi is just Sir Edmond Hillary in disguise, to find the true meaning of life, one must start burning my Vogels with the Tui, mate.", "link" => "Click me", "link-href" => "http:www.clickme.co.nz"), 2 => array("heading" => "Heading 3", "copy" => " Technology has allowed cool tiki tours to participate in the global conversation of random All Blacks. The next Generation of pretty suss sad guys have already cooked over at the fish n chip shop. Whats the hurry the Armed Offenders Squad? Theres plenty of Edmonds Cook Books in a waka.", "link" => "Click me", "link-href" => "http:www.clickme.co.nz"));
-        // line 126
+        // line 135
         echo "
     ";
-        // line 127
+        // line 136
         $context["content4Items"] = array(0 => array("heading" => "Heading 1", "copy" => "Anyway, Hairy Maclary from Donaldsons Dairy is just The Hungery Caterpilar in disguise, to find the true meaning of life, one must start chundering with the can of Watties Baked Beans, mate. After the vivid is munted, you add all the pearler wet blankets to the giant weka youve got yourself a meal. ", "link" => "Click me", "link-href" => "http:www.clickme.co.nz"), 1 => array("heading" => "Heading 2", "copy" => "You have no idea how nuclear-free our hard case stubbies were aye. Every time I see those good as cans of Watties Baked Beans its like smoko time all over again aye, Im not here to frack spiders. Anyway, Rangi is just Sir Edmond Hillary in disguise, to find the true meaning of life, one must start burning my Vogels with the Tui, mate.", "link" => "Click me", "link-href" => "http:www.clickme.co.nz"), 2 => array("heading" => "Heading 3", "copy" => " Technology has allowed cool tiki tours to participate in the global conversation of random All Blacks. The next Generation of pretty suss sad guys have already cooked over at the fish n chip shop. Whats the hurry the Armed Offenders Squad? Theres plenty of Edmonds Cook Books in a waka.", "link" => "Click me", "link-href" => "http:www.clickme.co.nz"), 3 => array("heading" => "Heading 4", "copy" => "Lake Taupo holds the most sweet community in the country.. James Cook was wobbling when the kiwi as rooting event occured. Fair suck of the sav, this beached as scarfie is as beaut as a hammered. Mean while, in South Pacific, James and the Giant Peach and Jonah Lomu were up to no good with a bunch of tapu whitebait fritters. ", "link" => "Click me", "link-href" => "http:www.clickme.co.nz"));
-        // line 149
+        // line 158
         echo "
     
 
     ";
-        // line 152
+        // line 161
         $context["galleryShowcase"] = array("title" => "Hello there", "url" => "/link/click/me", "imageSource" => "/assets/images/gallery-showcase.jpg", "imageCredit" => "Some photographer", "parentLink" => "/parent/link", "category" => "Television", "date" => "14 January 2014", "listingBlurb" => "Dont be a egg. After the fella is skived off, you add all the pretty suss cookie times to the foreshore and seabed issue youve got yourself a meal. Technology has allowed carked it housies to participate in the global conversation of hard case pieces of cheese on toast. The next Generation of nuclear-free manuses have already munted over at Rangitoto Island. Whats the hurry the Armed Offenders Squad? Theres plenty of jerseys in South Pacific..");
-        // line 163
+        // line 172
         echo "
 ";
-        // line 164
+        // line 173
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["navigation"])) { $_navigation_ = $context["navigation"]; } else { $_navigation_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "svg", 1 => $_navigation_), "method"), "html", null, true);
@@ -94,7 +97,7 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
         header.twig
     </h3>
     ";
-        // line 169
+        // line 178
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["navigation"])) { $_navigation_ = $context["navigation"]; } else { $_navigation_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "header", 1 => array("navigation" => $_navigation_, "isStacked" => "", "blockSize" => "m")), "method"), "html", null, true);
@@ -106,7 +109,7 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
         content.intro.twig
     </h3>
     ";
-        // line 176
+        // line 185
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["data"])) { $_data_ = $context["data"]; } else { $_data_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "content.intro", 1 => twig_array_merge($_data_, array("isStackedTop" => "", "isStackedBottom" => "", "blockSize" => "m"))), "method"), "html", null, true);
@@ -117,21 +120,21 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
         content.media.twig
     </h3>
     ";
-        // line 182
+        // line 191
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["data"])) { $_data_ = $context["data"]; } else { $_data_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "content.media", 1 => twig_array_merge($_data_, array("isStacked" => "", "blockSize" => "m", "mediaLeft" => "col-1-4", "mediaRight" => "col-3-4", "blockSize" => "m"))), "method"), "html", null, true);
         echo "
     <hr />
     ";
-        // line 184
+        // line 193
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["data"])) { $_data_ = $context["data"]; } else { $_data_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "content.media", 1 => twig_array_merge($_data_, array("isStacked" => "", "blockSize" => "m", "mediaLeft" => "col-1-3", "mediaRight" => "col-2-3", "blockSize" => "m"))), "method"), "html", null, true);
         echo "
     <hr />
     ";
-        // line 186
+        // line 195
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["data"])) { $_data_ = $context["data"]; } else { $_data_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "content.media", 1 => twig_array_merge($_data_, array("isStacked" => "", "blockSize" => "m", "mediaLeft" => "col-2-4", "mediaRight" => "col-2-4", "blockSize" => "m"))), "method"), "html", null, true);
@@ -145,21 +148,21 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
     </h3>
     <!-- I have passed in different nodes just to keep the site smaller ;) -->
     ";
-        // line 195
+        // line 204
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["gallery2Items"])) { $_gallery2Items_ = $context["gallery2Items"]; } else { $_gallery2Items_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "gallery.list", 1 => array("gallery" => $_gallery2Items_, "isStacked" => "", "blockSize" => "m", "colSize" => "col-2-4")), "method"), "html", null, true);
         echo "
     <hr/>
     ";
-        // line 197
+        // line 206
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["gallery3Items"])) { $_gallery3Items_ = $context["gallery3Items"]; } else { $_gallery3Items_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "gallery.list", 1 => array("gallery" => $_gallery3Items_, "isStacked" => "", "blockSize" => "m", "colSize" => "col-1-3")), "method"), "html", null, true);
         echo "
     <hr/>
     ";
-        // line 199
+        // line 208
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["gallery4Items"])) { $_gallery4Items_ = $context["gallery4Items"]; } else { $_gallery4Items_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "gallery.list", 1 => array("gallery" => $_gallery4Items_, "isStacked" => "", "blockSize" => "m", "colSize" => "col-1-4")), "method"), "html", null, true);
@@ -172,21 +175,21 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
         Content blocks
     </h3>
     ";
-        // line 207
+        // line 216
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["content2Items"])) { $_content2Items_ = $context["content2Items"]; } else { $_content2Items_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "content.block", 1 => array("content" => $_content2Items_, "isStacked" => "", "blockSize" => "m", "colSize" => "col-2-4", "headingSize" => "3")), "method"), "html", null, true);
         echo "
     <hr/>
     ";
-        // line 209
+        // line 218
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["content3Items"])) { $_content3Items_ = $context["content3Items"]; } else { $_content3Items_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "content.block", 1 => array("content" => $_content3Items_, "isStacked" => "", "blockSize" => "m", "colSize" => "col-1-3")), "method"), "html", null, true);
         echo "
     <hr/>
     ";
-        // line 211
+        // line 220
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["content4Items"])) { $_content4Items_ = $context["content4Items"]; } else { $_content4Items_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "content.block", 1 => array("content" => $_content4Items_, "isStacked" => "", "blockSize" => "m", "colSize" => "col-1-4")), "method"), "html", null, true);
@@ -197,7 +200,7 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
     <!--**************************** New block ****************************-->
 
 ";
-        // line 219
+        // line 228
         echo "
     <!--**************************** Showcases ****************************-->
     
@@ -206,7 +209,7 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
     </h3>
 
     ";
-        // line 226
+        // line 235
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["galleryShowcase"])) { $_galleryShowcase_ = $context["galleryShowcase"]; } else { $_galleryShowcase_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "gallery.showcase", 1 => array("galleryShowcase" => $_galleryShowcase_, "isStacked" => "", "blockSize" => "m")), "method"), "html", null, true);
@@ -217,10 +220,21 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
         Comment threads
     </h3>
     ";
-        // line 232
+        // line 241
         if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
         if (isset($context["commentThread"])) { $_commentThread_ = $context["commentThread"]; } else { $_commentThread_ = null; }
         echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "comments", 1 => array("commentThread" => $_commentThread_, "isStacked" => "", "blockSize" => "m", "colSize" => "col-2-4 col--q-small")), "method"), "html", null, true);
+        echo "
+
+    <!--**************************** Showcases ****************************-->
+    <h3 class='lego--heading'>
+        Panels
+    </h3>
+    ";
+        // line 247
+        if (isset($context["macros"])) { $_macros_ = $context["macros"]; } else { $_macros_ = null; }
+        if (isset($context["panel"])) { $_panel_ = $context["panel"]; } else { $_panel_ = null; }
+        echo twig_escape_filter($this->env, $this->getAttribute($_macros_, "lego", array(0 => "panel", 1 => array("panel" => $_panel_, "blockSize" => "m")), "method"), "html", null, true);
         echo "
 ";
     }
@@ -237,6 +251,6 @@ class __TwigTemplate_b10f9a503ee1aa5f55bc7ca611ae984e617a65daa571a80eddcb3d49a0b
 
     public function getDebugInfo()
     {
-        return array (  221 => 232,  210 => 226,  201 => 219,  190 => 211,  183 => 209,  176 => 207,  163 => 199,  156 => 197,  149 => 195,  135 => 186,  128 => 184,  121 => 182,  110 => 176,  98 => 169,  88 => 164,  85 => 163,  83 => 152,  78 => 149,  76 => 127,  73 => 126,  71 => 110,  68 => 109,  66 => 97,  63 => 96,  61 => 89,  58 => 88,  56 => 76,  53 => 75,  51 => 56,  48 => 55,  45 => 40,  42 => 29,  39 => 18,  37 => 8,  31 => 4,  28 => 3,);
+        return array (  235 => 247,  224 => 241,  213 => 235,  204 => 228,  193 => 220,  186 => 218,  179 => 216,  166 => 208,  159 => 206,  152 => 204,  138 => 195,  131 => 193,  124 => 191,  113 => 185,  101 => 178,  91 => 173,  88 => 172,  86 => 161,  81 => 158,  79 => 136,  76 => 135,  74 => 119,  71 => 118,  69 => 106,  66 => 105,  64 => 98,  61 => 97,  59 => 85,  56 => 84,  54 => 65,  51 => 64,  48 => 49,  45 => 38,  42 => 29,  39 => 18,  37 => 8,  31 => 4,  28 => 3,);
     }
 }
