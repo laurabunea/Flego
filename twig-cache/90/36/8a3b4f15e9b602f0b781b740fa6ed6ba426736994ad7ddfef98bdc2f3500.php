28121fd08e5fb1cbe83040f1bc1cd3bf1a0e1575
laurabunea <laura.r.bunea@gmail.com>
2014-04-16 22:46:47 +1200

    tidying up styles

--- twig-cache/90/36/8a3b4f15e9b602f0b781b740fa6ed6ba426736994ad7ddfef98bdc2f3500.php
+++ twig-cache/90/36/8a3b4f15e9b602f0b781b740fa6ed6ba426736994ad7ddfef98bdc2f3500.php
@@ -34,7 +34,7 @@ class __TwigTemplate_90368a3b4f15e9b602f0b781b740fa6ed6ba426736994ad7ddfef98bdc2
            <div class='grid__item col'>
                 <div class='block-m block--stacked'>
                     <h2 class='text-center font-primary--light'>
-                        Why make life harder when Fred can help
+                        Why make life harder when Fred can help :)
                     </h2>
                 </div>
             </div> 
@@ -42,7 +42,7 @@ class __TwigTemplate_90368a3b4f15e9b602f0b781b740fa6ed6ba426736994ad7ddfef98bdc2
     </div>
 </div>
 <div class='block--white'>
-    <div class='grid-container pt-xxl pb-xxl' > 
+    <div class='grid-container pt-xl pb-xl' > 
         <div class='grid grid--middle'>
            <div class='grid__item col-1-3 col--q-small'> 
                 <div class='block--stacked block-m'>
@@ -64,7 +64,7 @@ class __TwigTemplate_90368a3b4f15e9b602f0b781b740fa6ed6ba426736994ad7ddfef98bdc2
                     <p>
                         You include '_fred.scss' in your project, and customise its behavior in '_fred-theme.scss'. When there's an update to Fred, you can update it with a 'bower update' and get all the new Fred goodness in your project right away, without losing your settings. 
                     </p>
-                    <p>
+                    <p class='mt-s'>
                         <a href='http://springload.github.io/Fred.scss/#meet-fred-the-friendly-front-end-framework' class='btn'>
                             Read more about Fred.scss
                         </a>
@@ -76,7 +76,7 @@ class __TwigTemplate_90368a3b4f15e9b602f0b781b740fa6ed6ba426736994ad7ddfef98bdc2
 </div>
 
 <div class='block--featured'>
-    <div class='grid-container pt-xxl pb-xxl'>
+    <div class='grid-container pt-xl pb-xl'>
         <div class='grid grid--middle'>
             <div class='grid__item col-2-3 col--q-small'>
                 <div class='block--stacked block-m'>   
@@ -88,16 +88,11 @@ class __TwigTemplate_90368a3b4f15e9b602f0b781b740fa6ed6ba426736994ad7ddfef98bdc2
                         Flego is built of Fred.... add intro here
                     </p>
                     <p>
-                        The primary benefit of using design patterns is that we are not constantly re-inventing the wheel when coming up with the general functionality of a site or web application.
+                        The primary benefit of using design patterns is that we are not constantly re-inventing the wheel when coming up with the general functionality of a site.
                     </p>
                     <p>
                         It encouraged a much more modular approach to development, the idea being that any pattern could be used anywhere on a template.
-                    <p>
-                        <p>Promote consistency
-Promote modular thinking
-Add clarity to the parts and pieces that make up your website
-Increase efficiency when editing a website
-</p>
+                    <p class='mt-s'>
                         <a href='flego.php' class='btn'>
                             Read more about Flego
                         </a>
@@ -116,7 +111,7 @@ Increase efficiency when editing a website
 </div>
 
 <div class='block--white'>
-    <div class='grid-container pt-xxl pb-xxl' > 
+    <div class='grid-container pt-xl pb-xl' > 
         <div class='grid grid--middle'>
            <div class='grid__item col-1-3 col--q-small'> 
                 <div class='block--stacked block-m'>
@@ -133,7 +128,7 @@ Increase efficiency when editing a website
                         A place to store a sites dirty little secrets
                     </p>
                    
-                    <p>
+                    <p class='mt-s'>
                         <a href='/freds-closet.php' class='btn'>
                             Read more about Fred's closet
                         </a>
@@ -146,7 +141,7 @@ Increase efficiency when editing a website
 
 <!-- i know the anchor shouldn't wrap everything but it will do for now :0 -->
 ";
-        // line 146
+        // line 141
         echo "
 
 
@@ -168,6 +163,6 @@ Increase efficiency when editing a website
 
     public function getDebugInfo()
     {
-        return array (  150 => 146,  31 => 4,  28 => 3,);
+        return array (  145 => 141,  31 => 4,  28 => 3,);
     }
 }
