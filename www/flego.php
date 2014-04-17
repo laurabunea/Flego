7b8ac24ae8dbe6dd14582c27ea49b94aef26fa29
laurabunea <laura.r.bunea@gmail.com>
2014-04-17 12:04:30 +1200

    adding arrays to php

--- www/flego.php
+++ www/flego.php
@@ -1,4 +1,27 @@
 <?php 
 include_once __DIR__ . "/../init.php";
 
-echo $twig->render('flego.twig', array('the' => 'variables', 'go' => 'here'));
+$vars = array();
+
+
+$vars['contentBlock'] = array(
+    array(
+        "type" => "h1",
+        "data" => "I'm a heading!"
+    ),
+    array(
+        "type" => "h2",
+        "data" => "I'm a heading 2!"
+    ),
+    array(
+        "type" => "media",
+        "data" => array(
+            "I'm a heading!"
+            "I'm a heading!",
+            "I'm a heading!",
+        )
+    ),
+)
+
+
+echo $twig->render('flego.twig', array('data'=>$vars));
