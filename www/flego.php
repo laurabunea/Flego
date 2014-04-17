428a8ff21936908f753825c06a7fd5bd2bb6be8a
laurabunea <laura.r.bunea@gmail.com>
2014-04-17 16:39:20 +1200

    updating arraya in homepage

--- www/flego.php
+++ www/flego.php
@@ -25,6 +25,7 @@ $vars['contentblock'] = array(
         "data" => "A slice of heaven. Always blow on the pie. Ill see you right, this bung seabed is as bloody as a tapu Undie 500. Chur bro, we go together, kinda like mince n cheese ya know"
     ),
     array(
+        "type" => "content",
         "data" => "Oh no! No worries, this cool morepork is as kiwi as as a paru Undie 500. Across the ditch, good as gold, fully. Mean while, in behind the bicycle shed, Lomu and Helen Clarke were up to no good with a bunch of cracker vivids. The pretty suss force of his chundering was on par with John Keys heaps good Grandpas slipper. Put the jug on will you bro, all these wicked slippers can wait till later. The first prize for burning my Vogels goes to... the Armed Offenders Squad and his same same but different cookie time, what a dole bludger."
     )
               
--- www/index.php
+++ www/index.php
@@ -1,4 +1,39 @@
 <?php 
 include_once __DIR__ . "/../init.php";
 
-echo $twig->render('homepage.twig', array('siteClass' => '', 'go' => 'here'));
+$vars = array();
+
+$vars['fredcontent'] = array(
+    array(
+        "type" => "h1",
+        "data" => "Fred, the friendly front-end framework "
+    ),
+    array(
+        "type" => "intro",
+        "data" => "Fred helps jump-start your front-end projects. He's a super flexible little guy with a nice grunt-powered CLI. "
+    ),
+    array(
+        "type" => "content",
+        "data" => array(
+            "Fred aims to solve a classic front-end problem: sharing and re-using common components between projects, while allowing each project to retain custom variables and configuration.",
+            "You include '_fred.scss' in your project, and customise its behavior in '_fred-theme.scss'. When there's an update to Fred, you can update it with a 'bower update' and get all the new Fred goodness in your project right away, without losing your settings. "
+            )
+    ),
+    array(
+        "type" => "button",
+        "label" => "Read more about Fred.scss",
+        "href" => "http://springload.github.io/Fred.scss/#meet-fred-the-friendly-front-end-framework",
+    )
+);
+
+$vars['fredimage'] = array(
+    array(
+        "type" => "image-responsive",
+        "alt" => "alt text",
+        "title" => "image title",
+        "src" => "assets/images/fred-scss.jpg"
+    )
+              
+);
+
+echo $twig->render('homepage.twig', array('siteClass' => '','data' => $vars));
