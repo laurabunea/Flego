84220a337623a731d1b67a64fbdf4003c7562640
laurabunea <laura.r.bunea@gmail.com>
2014-04-17 16:22:41 +1200

    updates to array s

--- www/flego.php
+++ www/flego.php
@@ -15,6 +15,30 @@ $vars['article'] = array(
     )
 );
 
+$vars['contentblock'] = array(
+    array(
+        "type" => "h1",
+        "data" => "I am a title"
+    ),
+    array(
+        "type" => "intro",
+        "data" => "A slice of heaven. Always blow on the pie. Ill see you right, this bung seabed is as bloody as a tapu Undie 500. Chur bro, we go together, kinda like mince n cheese ya know"
+    ),
+    array(
+        "data" => "Oh no! No worries, this cool morepork is as kiwi as as a paru Undie 500. Across the ditch, good as gold, fully. Mean while, in behind the bicycle shed, Lomu and Helen Clarke were up to no good with a bunch of cracker vivids. The pretty suss force of his chundering was on par with John Keys heaps good Grandpas slipper. Put the jug on will you bro, all these wicked slippers can wait till later. The first prize for burning my Vogels goes to... the Armed Offenders Squad and his same same but different cookie time, what a dole bludger."
+    )
+              
+);
+
+$vars['mediaimage'] = array(
+ array(
+    "type" => "image-responsive",
+    "alt" => "alt text",
+    "title" => "image title",
+    "src" => "assets/images/gallery-showcase.jpg"
+    )
+              
+);
 
 echo $twig->render('flego.twig', array('data' => $vars));
 /*,
