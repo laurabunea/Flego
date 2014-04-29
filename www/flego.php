4f3de8360ed7d4d0a96ca5616b7b72695fbd3595
laurabunea <laura.r.bunea@gmail.com>
2014-04-30 11:07:33 +1200

    updated flego

--- www/flego.php
+++ www/flego.php
@@ -15,7 +15,6 @@ $vars['galleryItems'] = array(
             "alt" => "alt text"
         ),
         "data" => "I am a paragraph of text "
-        
     ),
     array(
         array(
@@ -94,7 +93,7 @@ $vars['flegoContent'] = array(
     ),
      array(
         "type" => "p",
-        "data" => "i am a p"
+        "data" => "These blocks should be used for starting purposes only. Once you recognise a pattern you should make specific blocks so you dont have  to repeat the code"
     )       
 );
 
@@ -107,16 +106,41 @@ $vars['flegoImage'] = array(
         )           
 );
 
-echo $twig->render('flego.twig', array('data' => $vars));
-/*,
+$vars['dummyImage'] = array(
     array(
-        "type" => "media",
-        "data" => array(
-            "I'm a heading!",
-            "I'm a heading!",
-            "I'm a heading!",
-        ),
+        "type" => "image-responsive",
+        "alt" => "Flego",
+        "title" => "portrait",
+        "src" => "assets/images/portrait.jpg"
+        )           
+);
+
+
+$vars['panel'] = array(
     array(
-        "data" => "A slice of heaven. Always blow on the pie. Ill see you right, this bung seabed is as bloody as a tapu Undie 500. Chur bro, we go together, kinda like mince n cheese ya know"
+        "type" => "h2",
+        "data" => "Get in touch",
+        "class" => "font-primary--light"
+    ),
+    "data" => "We would love to hear from you, you attractive person you."
+     
+);
+
+$vars['panelButton'] = array(
+    array(
+        "type" => "button",
+        "label" => "Click me",
+        "href" => "/clickme",
+        "class" => "right col border-box mt-m--q-small"
     )
-    )*/
+     
+);
+$vars['dummyContent'] = array(
+    array(
+        "type" => "h2",
+        "data" => "I am a title"
+    ),
+    "data" => "Kia ora.. You're not in Guatemala now, this rip-off kumara is as sweet as as a sweet as scarfie. Mean while, in Shortland Street, Manus Morissette and The Hungery Caterpilar were up to no good with a bunch of beached as whitebait fritters. "     
+);
+
+echo $twig->render('flego.twig', array('data' => $vars));
