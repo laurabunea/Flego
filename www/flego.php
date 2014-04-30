2b55cde563d6de6ef8999605130ad9cd5fcd33c8
laurabunea <laura.r.bunea@gmail.com>
2014-04-30 14:44:17 +1200

    added forms to flego

--- www/flego.php
+++ www/flego.php
@@ -85,6 +85,64 @@ $vars['mediaimage'] = array(
               
 );
 
+$vars['formData'] = array(
+    array(
+        array(
+            "type" => "label",
+            "for" => "firstname",
+            "data" => "First name",
+            "class" => "col"
+        ),
+        array(
+            "type" => "input",
+            "placeholder" => "type first name here",
+            "id" => "firstname",
+            "class" => "col text-input"
+        )
+    ),
+    array(
+        array(
+            "type" => "label",
+            "for" => "lastname",
+            "data" => "Last name",
+            "class" => "col "
+        ),
+        array(
+            "type" => "input",
+            "placeholder" => "type last name here",
+            "id" => "lastname",
+            "class" => "col text-input"
+        )
+    ),
+    array(
+        array(
+            "type" => "label",
+            "for" => "Passwor",
+            "data" => "Email address",
+            "class" => "col "
+        ),
+        array(
+            "type" => "input",
+            "placeholder" => "type email here",
+            "id" => "email",
+            "class" => "col text-input"
+        )
+    ),
+    array(
+        array(
+            "type" => "label",
+            "for" => "Passwor",
+            "data" => "Email address",
+            "class" => "col"
+        ),
+        array(
+            "type" => "input",
+            "placeholder" => "type email here",
+            "id" => "email",
+            "class" => "col text-input"
+        )
+    )          
+);
 
 $vars['flegoContent'] = array(
     array(
