7d74bfcc3bb68f9c2eab31d25055a4284779a7ae
laurabunea <laura.r.bunea@gmail.com>
2014-05-14 11:27:56 +1200

    updated code so the flego blocks data was getting pulled in

---
+++ www/dummyData/page1.php
@@ -0,0 +1,132 @@
+<?php 
+
+$vars['introSection'] = array(
+    "leftCopy" => array(
+        array(
+            "type" => "image-responsive",
+            "alt" => "I am some copy",
+            "src" => "/assets/images/iphone.png"
+        )
+    ),
+    "rightCopy" => array(
+
+        array(
+            "type" => "h1",
+            "class" => "h0",
+            "data" => "Hello."
+        ),
+        array(
+            "type" => "intro",
+            "data" => "Technology has allowed cool blokes to participate in the global conversation of bung stubbies. The next Generation of heaps good sad guys have already skived off over at the tinny house. "
+        )
+    )        
+);
+//breadcrumbs is just dummy breadcrumbs from flego 
+
+$vars['aboutUsSection'] = array(
+    array(
+        "type" => "h2",
+        "data" => "About us"
+    ),
+    array(
+        "type" => "intro",
+        "data" => "James Cook was cooking up a feed when the tapu chundering event occured. Au, this sweet as hokey pokey is as pretty suss as a beached as Undie 500. Mean while, in the pub, Jonah Lomu and The Topp Twins were up to no good with a bunch of wicked Longest Drink in Towns."
+    )
+);
+
+$vars['aboutUsGallery'] = array(
+    array(
+        
+        array(
+            "type" => "image-responsive",
+            "src" => "assets/images/placeholder3.jpg",
+            "alt" => "alt text"
+        ),
+        array(
+            "type" => "h4",
+            "data" => "Mr Coolio"
+        ),
+        "data" => "I am a paragraph of text ",
+        array(
+            "type" => "image",
+            "src" => "assets/images/facebook.png",
+            "alt" => "alt text",
+            "class" => "mt-xs"
+        ),
+    ),
+    array(
+        
+        array(
+            "type" => "image-responsive",
+            "src" => "assets/images/placeholder2.jpg",
+            "alt" => "alt text"
+        ),
+
+        array(
+            "type" => "h4",
+            "data" => "Mrs Thinkalot"
+        ),
+        "data" => "Second para",
+        array(
+            "type" => "image",
+            "src" => "assets/images/facebook.png",
+            "alt" => "alt text",
+            "class" => "mt-xs"
+        ),
+        array(
+            "type" => "image",
+            "src" => "assets/images/twitter.png",
+            "alt" => "alt text",
+            "class" => "mt-xs"
+        )
+        
+    ),
+    array(
+        array(
+            "type" => "image-responsive",
+            "src" => "assets/images/placeholder1.jpg",
+            "alt" => "alt text"
+        ),
+        array(
+            "type" => "h4",
+            "data" => "Miss Tired"
+        ),
+        "data" => "I am a paragraph of text ",
+        array(
+            "type" => "image",
+            "src" => "assets/images/facebook.png",
+            "alt" => "alt text",
+            "class" => "mt-xs"
+        ),
+        array(
+            "type" => "image",
+            "src" => "assets/images/google.png",
+            "alt" => "alt text",
+            "class" => "mt-xs"
+        )
+    )   
+);
+
+$vars['portfolioSection'] = array(
+    array(
+        "type" => "h2",
+        "data" => "Portfolio"
+    ),
+    array(
+        "type" => "intro",
+        "data" => "James Cook was cooking up a feed when the tapu chundering event as a beached as Undie 500. Mean while, in the pub, Jonah Lomu and The Topp Twins were up to no good with a bunch of wicked Longest Drink in Towns."
+    )
+);
+
+$vars['contactSection'] = array(
+    array(
+        "type" => "h2",
+        "data" => "Contact Us"
+    ),
+    array(
+        "type" => "intro",
+        "data" => "Drop me a message"
+    ) 
+);
+
+?>
